0767d5dfcb73359e262d8deea4598bc4d0e62293
aziz.mammadov <edunya002@gmail.com>
2025-03-06 14:38:36 +0400

    06.03

---
+++ src/Quiz2/SwapModifyWithArray.java
@@ -0,0 +1,45 @@
+package Quiz2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SwapModifyWithArray {
+    public static void main(String[] args) {
+
+        /* İstifadəçidən iki ədəd massiv daxil edin (hər biri 5 ədəd olmalıdır). Hər iki massivdəki ədədləri
+            bir-biri ilə mübadilə edin (yəni, massivlərin elementlərini dəyişdirin)*/
+
+Scanner sc = new Scanner(System.in);
+/*
+Integer[] arr1 = new Integer[5];
+Integer[] arr2 = new Integer[5];
+
+System.out.println("Enter the elements of the FirstArray " );
+for (int i = 0; i < arr1.length; i++) {
+    arr1[i] = sc.nextInt();
+    }
+        System.out.println("Enter the elements of the SecondArray " );
+for (int i = 0; i < arr2.length; i++) {
+    arr2[i] = sc.nextInt();
+  }
+System.out.println("First  Array is  : " + Arrays.toString(arr1));
+System.out.println("Second Array is  : " + Arrays.toString(arr2));
+
+for (int i = 0; i < arr1.length; i++) {
+    int temp = arr1[i];
+    arr1[i] = arr2[i];
+    arr2[i] = temp;
+}
+System.out.println("NewFirst  Array is  : " + Arrays.toString(arr1));
+System.out.println("NewSecond Array is  : " + Arrays.toString(arr2));
+
+*/
+
+
+
+
+
+
+
+    }
+}
