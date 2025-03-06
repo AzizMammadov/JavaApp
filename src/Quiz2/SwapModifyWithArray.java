9efe44db87f808e0d9956c75fa18eb9b8ae82797
aziz.mammadov <edunya002@gmail.com>
2025-03-06 15:12:30 +0400

    06.03

--- src/Quiz2/SwapModifyWithArray.java
+++ src/Quiz2/SwapModifyWithArray.java
@@ -12,17 +12,18 @@ public class SwapModifyWithArray {
 Scanner sc = new Scanner(System.in);
 
 Integer[] arr1 = new Integer[5];
-Integer[] arr2 = new Integer[5];
+// Integer[] arr2 = new Integer[5];
 
 System.out.println("Enter the elements of the FirstArray " );
 for (int i = 0; i < arr1.length; i++) {
     arr1[i] = sc.nextInt();
     }
-        System.out.println("Enter the elements of the SecondArray " );
+System.out.println("First  Array is  : " + Arrays.toString(arr1));
+ /*       System.out.println("Enter the elements of the SecondArray " );
 for (int i = 0; i < arr2.length; i++) {
     arr2[i] = sc.nextInt();
   }
-System.out.println("First  Array is  : " + Arrays.toString(arr1));
+
 System.out.println("Second Array is  : " + Arrays.toString(arr2));
 
 for (int i = 0; i < arr1.length; i++) {
@@ -33,10 +34,43 @@ for (int i = 0; i < arr1.length; i++) {
 System.out.println("NewFirst  Array is  : " + Arrays.toString(arr1));
 System.out.println("NewSecond Array is  : " + Arrays.toString(arr2));
 
+*/
 
+       /* Bir massiv yaradın və həmin massivdəki hər bir ədədin yerini dəyişin (məsələn, ilk və sonuncu
+                elementi mübadilə edin, sonra ikinci və sondan birinci elementi və s.).*/
 
+/*
+for (int i = 0; i < arr1.length; i++) {
+    int temp = arr1[i];
+    arr1[i] = arr1[arr1.length-1-i];
+    arr1[arr1.length-1-i] = temp;
+}
+System.out.println("          Array  : " + Arrays.toString(arr1));
+*/
 
+      /*  Bir massiv yaradın və həmin massivdəki ədədləri ikiqat artırın.
+       Məsələn, 1, 2, 3 massivini 2, 4, 6-ya çevirin. */
+/*
+        for (int i = 0; i < arr1.length; i++) {
+            int temp = arr1[i] *2 ;
+arr1[i] = temp;
+        }
+System.out.println(" Array   : " + Arrays.toString(arr1));
 
+*/
+
+/* Bir massivdəki iki ədədin yerini dəyişin. İstifadəçidən massivdə iki index daxil edin və həmin
+indexlərdəki ədədləri bir-birinə mübadilə edin*/
+
+        System.out.print("Massivde degisecek 1-ci indexi daxil edin  : ");
+int FirstIndex = sc.nextInt();
+        System.out.print("Massivde degisecek 2-ci indexi daxil edin  : ");
+int SecondIndex = sc.nextInt();
+int temp = arr1[FirstIndex];
+arr1[FirstIndex] = arr1[SecondIndex];
+arr1[SecondIndex] = temp;
+
+System.out.println("Array is  : " + Arrays.toString(arr1));
 
 
 
