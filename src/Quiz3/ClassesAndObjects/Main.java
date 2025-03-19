9fa0ccccc322c89d7a1d44e2ed0ae954ee2bb08d
aziz.mammadov <edunya002@gmail.com>
2025-03-19 16:54:40 +0400

    19.03

---
+++ src/Quiz3/ClassesAndObjects/Main.java
@@ -0,0 +1,11 @@
+package Quiz3.ClassesAndObjects;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Person person = new Person(" Aziz ", 25);
+    person.introduce();
+    
+
+}
+}
